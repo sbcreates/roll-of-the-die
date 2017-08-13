40fc846eb57d56722d841f6a41ccdc7ab0e2225e
sbcreates <drawsarahb@gmail.com>
2017-08-13 16:53:15 -0400

    Completed part 2 of exercise

--- roll_die_ten_times.rb
+++ roll_die_ten_times.rb
@@ -1,9 +1,12 @@
 def random_six(string)
-  return 1 + rand(6)
+  1 + rand(6)
 end
 
 puts "Type 'roll' then enter to roll the dice"
 
 user_roll = gets.to_s
 
-p random_six(user_roll)
+10.times do
+  result = random_six(user_roll)
+  puts "The result of your roll is #{result}"
+end
