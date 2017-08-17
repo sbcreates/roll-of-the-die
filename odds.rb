c252c5a0e2f518167fb870a01ee4fb9f30b615a1
sbcreates <drawsarahb@gmail.com>
2017-08-17 14:06:12 -0400

    Completed and tested odds exercise

--- odds.rb
+++ odds.rb
@@ -1,15 +1,22 @@
 permutation_totals = {}
+sum = 0
+
 
 (6...25).each do |number|
   permutation_totals[number] = 0
 end
-#outputs {2=>0, 3=>0, 4=>0, 5=>0, 6=>0, 7=>0, 8=>0, 9=>0, 10=>0, 11=>0, 12=>0}
+#outputs {6=>0, 7=>0, 8=>0, 9=>0, 10=>0, 11=>0, 12=>0, 13=>0, 14=>0, 15=>0, 16=>0, 17=>0, 18=>0, 19=>0, 20=>0, 21=>0, 22=>0, 23=>0, 24=>0}
 
 (3...13).each do |piece1|
   (3...13).each do |piece2|
     permutation_totals[piece1 + piece2] += 1
+    sum += 1
   end
 end
-#outputs {2=>1, 3=>2, 4=>3, 5=>4, 6=>5, 7=>6, 8=>5, 9=>4, 10=>3, 11=>2, 12=>1}
+#outputs {6=>1, 7=>2, 8=>3, 9=>4, 10=>5, 11=>6, 12=>7, 13=>8, 14=>9, 15=>10, 16=>9, 17=>8, 18=>7, 19=>6, 20=>5, 21=>4, 22=>3, 23=>2, 24=>1}
 
-permutation_totals.each { |key, value| p "#{key} occurs #{value} times" }
+permutation_totals.each do |key, value|
+  odds = value.to_f
+  sum * 100
+  puts "The odds of #{key} occuring are #{odds}%"
+end
